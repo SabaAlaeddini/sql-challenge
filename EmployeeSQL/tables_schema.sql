c90cb90c996cb7755e7e05ec3eca4f1c6a2ea89a
SabaAlaeddini <ba_dini_70@yahoo.com>
2025-01-12 14:11:25 -0500

    final file added

---
+++ EmployeeSQL/tables_schema.sql
@@ -0,0 +1,98 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+
+
+CREATE TABLE "titles" (
+    "title_id" varchar(5)   NOT NULL,
+    "title" varchar(20)   NOT NULL,
+    CONSTRAINT "pk_titles" PRIMARY KEY (
+        "title_id"
+     )
+);
+
+SELECT * FROM titles;
+
+
+
+CREATE TABLE "employees" (
+    "emp_no" int   NOT NULL,
+    "emp_title_id" varchar(5)   NOT NULL,
+    "birth_date" date   NOT NULL,
+    "first_name" varchar(15)   NOT NULL,
+    "last_name" varchar(20)   NOT NULL,
+    "sex" varchar(1)   NOT NULL,
+    "hire_date" date   NOT NULL,
+    CONSTRAINT "pk_employees" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+SELECT * FROM employees;
+
+
+
+CREATE TABLE "departments" (
+    "dept_no" varchar(5)   NOT NULL,
+    "dept_name" varchar(20)   NOT NULL,
+    CONSTRAINT "pk_departments" PRIMARY KEY (
+        "dept_no"
+     )
+);
+
+SELECT * FROM departments;
+
+
+
+CREATE TABLE "dept_emp" (
+    "emp_no" int   NOT NULL,
+    "dept_no" varchar(5)   NOT NULL,
+    CONSTRAINT "pk_dept_emp" PRIMARY KEY (
+        "emp_no","dept_no"
+     )
+);
+
+SELECT * FROM dept_emp;
+
+
+
+CREATE TABLE "dept_manager" (
+    "dept_no" varchar(5)   NOT NULL,
+    "emp_no" int   NOT NULL,
+    CONSTRAINT "pk_dept_manager" PRIMARY KEY (
+        "dept_no","emp_no"
+     )
+);
+
+SELECT * FROM dept_manager;
+
+
+
+
+CREATE TABLE "salaries" (
+    "emp_no" int   NOT NULL,
+    "salary" int   NOT NULL,
+    CONSTRAINT "pk_salaries" PRIMARY KEY (
+        "emp_no","salary"
+     )
+);
+
+SELECT * FROM salaries;
+
+
+
+ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_title_id" FOREIGN KEY("emp_title_id")
+REFERENCES "titles" ("title_id");
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # sql-challenge
 Module 9 Challenge
+
+# Overview
+This Challenge is divided into three parts: data modeling, data engineering, and data analysis.
+
+# Data Modeling
+The six CSV files for the project is downloaded from "Module 9 Challenge files" and stored in "data" folder
+In this part, “https://www.quickdatabasediagrams.com/" is used for an Entity Relationship Diagram of the requiered tables. The output is in the EmployeesSQL folder, saved as QuickDBD-Free Diagram.
+
+# Data Engineering
+By following the instructions, table schema is created for each of the six csv files. Each of the tables include a primary key, foreign keys, composite key (when necessary), datatype and other constraints. After creating the tables, the csv files is imported into its corresponding SQL table, then each of the tables output is saved in to the EmployeesSQL folder.
+
+# Data Analysis
+The data_analysis_queries is written in order to answer the requested data anayisis:
+
+List the employee number, last name, first name, sex, and salary of each employee.
+
+List the first name, last name, and hire date for the employees who were hired in 1986.
+
+List the manager of each department along with their department number, department name, employee number, last name, and first name.
+
+List the department number for each employee along with that employee’s employee number, last name, first name, and department name.
+
+List first name, last name, and sex of each employee whose first name is Hercules and whose last name begins with the letter B.
+
+List each employee in the Sales department, including their employee number, last name, and first name.
+
+List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name.
+
+List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name).
